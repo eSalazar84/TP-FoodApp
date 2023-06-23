1f4466f24f8892a28ffd429b8a58746e436085dd
Emiliano Salazar <salazaremiliano84@gmail.com>
2023-06-22 21:55:20 -0300

    added npm react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './styles/App.css';
 import Header from './views/Header.jsx';
 import Main from './views/Main.jsx';
 import Footer from './views/Footer.jsx';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
